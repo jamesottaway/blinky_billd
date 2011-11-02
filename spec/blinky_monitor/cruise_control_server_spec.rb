741328e3b7c44c87f14483d7a7c2351edbdf7b14
James Ottaway <james@ottaway.mp>
2011-11-03 08:14:38 +1000

    Cleaned up CruiseControlServer specs

--- spec/blinky_monitor/cruise_control_server_spec.rb
+++ spec/blinky_monitor/cruise_control_server_spec.rb
@@ -2,67 +2,53 @@ require File.dirname(__FILE__)+'/../rspec_helper'
 require 'blinky_monitor/cruise_control_server'
 
 describe BlinkyMonitor::CruiseControlServer do
-  before do
-    @server = BlinkyMonitor::CruiseControlServer.new 'build'
+  context 'success' do
+    let(:response) { feed_for 'cruise_control_success.xml' }
+    let(:stream) { stub 'stream', :read => response }
+
+    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
+
+    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
+    its(:status) { should == :success }
   end
   
-  it 'should return success if the project is successful' do
-    response = <<EOF
-    <Projects>
-<Project webUrl="http://build/job/green/" name="green" lastBuildLabel="1" lastBuildTime="2011-12-30T12:00:00Z" lastBuildStatus="Success" activity="Sleeping"/>
-</Projects>
-EOF
-    stream = stub 'stream', :read => response
-    @server.should_receive(:open).with('build/cc.xml').and_return(stream)
-    @server.status.should == :success
-  end
-  
-  it 'should return failure if the projects fails' do
-    response = <<EOF
-    <Projects>
-<Project webUrl="http://build/job/red/" name="red" lastBuildLabel="1" lastBuildTime="2011-12-30T12:00:00Z" lastBuildStatus="Failure" activity="Sleeping"/>
-</Projects>
-EOF
-    
-    stream = stub 'stream', :read => response
-    @server.should_receive(:open).with('build/cc.xml').and_return(stream)
-    @server.status.should == :failure
+  context 'failure' do
+    let(:response) { feed_for 'cruise_control_failure.xml' }
+    let(:stream) { stub 'stream', :read => response }
+
+    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
+
+    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
+    its(:status) { should == :failure }
   end
 
-  it 'should return failure if one project is successful but another is failing' do
-    response = <<EOF
-    <Projects>
-<Project webUrl="http://build/job/green/" name="green" lastBuildLabel="1" lastBuildTime="2011-12-30T12:00:00Z" lastBuildStatus="Success" activity="Sleeping"/>
-<Project webUrl="http://build/job/red/" name="red" lastBuildLabel="1" lastBuildTime="2011-12-30T12:00:00Z" lastBuildStatus="Failure" activity="Sleeping"/>
-</Projects>
-EOF
-    
-    stream = stub 'stream', :read => response
-    @server.should_receive(:open).with('build/cc.xml').and_return(stream)
-    @server.status.should == :failure
+  context 'failure > success' do
+    let(:response) { feed_for 'cruise_control_success_and_failure.xml' }
+    let(:stream) { stub 'stream', :read => response }
+
+    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
+
+    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
+    its(:status) { should == :failure }
   end
 
-  it 'should return building if a previously successful project is building' do
-    response = <<EOF
-    <Projects>
-<Project webUrl="http://build/job/green/" name="green" lastBuildLabel="1" lastBuildTime="2011-12-30T12:00:00Z" lastBuildStatus="Success" activity="Building"/>
-</Projects>
-EOF
-    
-    stream = stub 'stream', :read => response
-    @server.should_receive(:open).with('build/cc.xml').and_return(stream)
-    @server.status.should == :building
+  context 'building > success' do
+    let(:response) { feed_for 'cruise_control_building_after_success.xml' }
+    let(:stream) { stub 'stream', :read => response }
+
+    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
+
+    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
+    its(:status) { should == :building }
   end
 
-  it 'should return building if a previously failed project is building' do
-    response = <<EOF
-    <Projects>
-<Project webUrl="http://build/job/green/" name="green" lastBuildLabel="1" lastBuildTime="2011-12-30T12:00:00Z" lastBuildStatus="Failure" activity="Building"/>
-</Projects>
-EOF
-    
-    stream = stub 'stream', :read => response
-    @server.should_receive(:open).with('build/cc.xml').and_return(stream)
-    @server.status.should == :building
+  context 'building > failure' do
+    let(:response) { feed_for 'cruise_control_building_after_failure.xml' }
+    let(:stream) { stub 'stream', :read => response }
+
+    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
+
+    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
+    its(:status) { should == :building }
   end
 end
