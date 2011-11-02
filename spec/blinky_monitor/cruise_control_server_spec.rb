c4751ae5998f1ed5ed707585f63d0cfc3ae7ba46
James Ottaway <james@ottaway.mp>
2011-11-03 08:21:20 +1000

    Whoa, ever DRYer specs

--- spec/blinky_monitor/cruise_control_server_spec.rb
+++ spec/blinky_monitor/cruise_control_server_spec.rb
@@ -2,53 +2,39 @@ require File.dirname(__FILE__)+'/../rspec_helper'
 require 'blinky_monitor/cruise_control_server'
 
 describe BlinkyMonitor::CruiseControlServer do
-  context 'success' do
-    let(:response) { feed_for 'cruise_control_success.xml' }
+  shared_context :cruise_control do
     let(:stream) { stub 'stream', :read => response }
-
     subject { BlinkyMonitor::CruiseControlServer.new 'build' }
-
     before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
+  end
+
+  context 'success' do
+    include_context :cruise_control
+    let(:response) { feed_for 'cruise_control_success.xml' }
     its(:status) { should == :success }
   end
   
   context 'failure' do
+    include_context :cruise_control
     let(:response) { feed_for 'cruise_control_failure.xml' }
-    let(:stream) { stub 'stream', :read => response }
-
-    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
-
-    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
     its(:status) { should == :failure }
   end
 
   context 'failure > success' do
+    include_context :cruise_control
     let(:response) { feed_for 'cruise_control_success_and_failure.xml' }
-    let(:stream) { stub 'stream', :read => response }
-
-    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
-
-    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
     its(:status) { should == :failure }
   end
 
   context 'building > success' do
+    include_context :cruise_control
     let(:response) { feed_for 'cruise_control_building_after_success.xml' }
-    let(:stream) { stub 'stream', :read => response }
-
-    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
-
-    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
     its(:status) { should == :building }
   end
 
   context 'building > failure' do
+    include_context :cruise_control
     let(:response) { feed_for 'cruise_control_building_after_failure.xml' }
-    let(:stream) { stub 'stream', :read => response }
-
-    subject { BlinkyMonitor::CruiseControlServer.new 'build' }
-
-    before { subject.should_receive(:open).with('build/cc.xml').and_return(stream) }
     its(:status) { should == :building }
   end
 end
--- spec/blinky_monitor/jenkins_server_spec.rb
+++ spec/blinky_monitor/jenkins_server_spec.rb
@@ -2,23 +2,21 @@ require File.dirname(__FILE__)+'/../rspec_helper'
 require 'blinky_monitor/jenkins_server'
 
 describe BlinkyMonitor::JenkinsServer do
-  context 'success' do
-    let(:response) { feed_for 'jenkins_success.xml' }
+  shared_context :jenkins do
     let(:stream) { stub 'stream', :read => response }
-
     subject { BlinkyMonitor::JenkinsServer.new 'build' }
-
     before { subject.should_receive(:open).with('build/rssLatest').and_return(stream) }
+  end
+
+  context 'success' do
+    include_context :jenkins
+    let(:response) { feed_for 'jenkins_success.xml' }
     its(:status) { should == :success }
   end
 
   context 'failure' do
+    include_context :jenkins
     let(:response) { feed_for 'jenkins_failure.xml' }
-    let(:stream) { stub 'stream', :read => response }
-
-    subject { BlinkyMonitor::JenkinsServer.new 'build' }
-
-    before { subject.should_receive(:open).with('build/rssLatest').and_return(stream) }
     its(:status) { should == :failure }
   end
 end
