8ce2ac4a42ed389c9e3216c353fa05878f860331
James Ottaway <james@ottaway.mp>
2012-04-17 09:12:27 +1000

    Fixed small issue in Controller which needed to pop the arg of the args array

--- lib/blinky_billd/controller.rb
+++ lib/blinky_billd/controller.rb
@@ -3,7 +3,8 @@ require 'billd/status'
 
 module BlinkyBilld
 	class Controller
-		def self.run uri
+		def self.run args
+			uri = args.shift
 			blinky = Blinky.new
 			billd_status = Billd::Status.new(uri)
 			blinky.light.send(billd_status.overall)
